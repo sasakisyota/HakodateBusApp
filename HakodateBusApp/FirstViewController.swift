b81875ede5ae1f96701b4eb46f915cfaca72d5e3
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-11-04 15:37:20 +0900

    add Autolayout

--- HakodateBusApp/FirstViewController.swift
+++ HakodateBusApp/FirstViewController.swift
@@ -9,27 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SearchView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        UITabBar.appearance().tintColor = UIColor.black
-        UITabBar.appearance().barTintColor = UIColor.red
-        // Do any additional setup after loading the view.
-        
-        let theView = UIView()
-        theView.frame = CGRect(x: 60, y: 100, width: 210, height: 200)
-        theView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.6, alpha: 1.0)
-        
-        let labelB = UILabel()
-        labelB.text = "ラベルB"
-        
-        labelB.frame = CGRect(x: 50, y: 60, width: 110, height: 21)
-        
-        labelB.backgroundColor = UIColor.orange
-        labelB.textColor = UIColor.white
-        
-        theView.addSubview(labelB)
-        self.view.addSubview(theView)
-        
+       
     }
     
 
