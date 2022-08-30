75ed189af701bfbab0778ecd7877e64030432e54
sasakisyota <sasakisyota@icloud.o>
2022-08-30 15:37:17 +0900

    add new file

---
+++ HakodateBusApp/viewController.swift
@@ -0,0 +1,17 @@
+//
+//  viewController.swift
+//  HakodateBusApp
+//
+//  Created by 佐々木翔太 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SearchView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
